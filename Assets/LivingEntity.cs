02ae2bb93f6cc48f2b60b382338482037a328963
Gaelan Steele <gbs@canishe.com>
2017-02-01 14:49:33 -0800

    Initial commit

---
+++ Assets/LivingEntity.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class LivingEntity : MonoBehaviour, IDamageable {
+
+	 //int Score = 0;
+
+	public float startingHealth;
+	protected float health;
+	protected bool dead;
+
+	public event System.Action OnDeath;
+	protected virtual void Start() {// this is saying heath = stating heath so if you dont have a living entity
+		health = startingHealth;	//you can still have heath
+	}
+
+
+	public void TakeHit(float damage, RaycastHit hit) {
+		// Do some stuff here with hit var
+		TakeDamage (damage);
+	}
+		
+	public void TakeDamage(float damage) {
+		health -= damage;
+		if (health <= 0 && !dead) {
+			Die();
+		}
+	}
+	protected void Die() {
+		dead = true;
+		if (OnDeath != null) {
+			OnDeath();
+		}
+		//Score + 1;   // here is where you can keep trak of how many deaths there has been.
+		GameObject.Destroy (gameObject);
+
+	}
+
+}
---
+++ Assets/dUNCELORIEL/Animatro.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animatro : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.W)) {
+			anim.SetInteger ("State", 1);
+		} else if (Input.GetKeyUp (KeyCode.W)) {
+			anim.SetInteger ("State", 0);
+		} else if (Input.GetKeyDown (KeyCode.S)) {
+			anim.SetInteger ("State", 3);
+		} else if (Input.GetKeyUp (KeyCode.S)) {
+			anim.SetInteger ("State", 0);
+		} else if (Input.GetKeyDown (KeyCode.D)) {
+			anim.SetInteger ("State", 2);
+		} else if (Input.GetKeyUp (KeyCode.D)) {
+			anim.SetInteger ("State", 0);
+		} else if (Input.GetKeyDown (KeyCode.A)) {
+			anim.SetInteger ("State", 4);
+		} else if (Input.GetKeyUp (KeyCode.A)) {
+			anim.SetInteger ("State", 0);
+		}
+	}
+}
